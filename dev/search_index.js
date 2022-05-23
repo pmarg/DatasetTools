a8c3b31c8ae38763bc42101f0eae1e6d01766203
Documenter.jl <documenter@juliadocs.github.io>
2022-05-23 17:11:42 +0000

    build based on 84b544c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DatasetTools","category":"page"},{"location":"#DatasetTools","page":"Home","title":"DatasetTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DatasetTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DatasetTools]","category":"page"}]
+}
